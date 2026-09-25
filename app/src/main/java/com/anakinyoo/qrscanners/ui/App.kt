6d4e5ab910b8d44d879abf770d4eaf251fb352d3
AnakinYoo <215334638+dachopol@users.noreply.github.com>
2026-09-25 21:09:17 +0700

    fix: import heightIn for privacy dialog

--- app/src/main/java/com/anakinyoo/qrscanners/ui/App.kt
+++ app/src/main/java/com/anakinyoo/qrscanners/ui/App.kt
@@ -50,6 +50,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
